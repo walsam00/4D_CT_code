68717df44a030fb873a1d754a5d1a1f8428e056f
walsam00 <83233725+walsam00@users.noreply.github.com>
2022-05-11 09:45:24 +0200

    Update UNet_3D_v2_inference_file_prep.py

--- UNet_3D_v2_inference_file_prep.py
+++ UNet_3D_v2_inference_file_prep.py
@@ -1,17 +1,20 @@
+# This code takes the reconstructed CT image stacks from a specified directory, 
+# bins them to size 243x243 (x,y) and saves them in a new path for further processing
+
 import numpy as np
 import h5py
 import getopt, sys
-import skimage.transform
-#import os
 import numba
 from numba import jit
+
 #get command line arguments for which tablet should be processed
+#this code is called by a SLURM script so this is a useful way of populating the variables
 
 full_cmd_arguments = sys.argv
 argument_list = full_cmd_arguments[1:]
 
-short_options = "s:o:x:f:m:h"
-long_options = ["input_dir=", "output_dir=", "max_index=", "folder_name=", "model_path=", "help"]
+short_options = "x:f:"
+long_options = ["max_index=", "folder_name="]
 
 try:
 	arguments, values = getopt.getopt(argument_list, short_options, long_options)
@@ -20,32 +23,19 @@ except getopt.error as err:
 	sys.exit(2)
 
 for current_argument, current_value in arguments:
-	if current_argument in ("-s", "--ssh"):
-		ssh_bool = int(current_value)
-		if ssh_bool == 0:
-			in_dir = '/mnt/nfs-scicore/time_resolved_uCT_data_PSI/'
-		else:
-			in_dir = '/scicore/projects/pharmatech-scicore/time_resolved_uCT_data_PSI/'
-	if current_argument in ("-o", "--output_dir"):
-		out_dir = str(current_value)
 	if current_argument in ("-x", "--max_index"):
 		max_index = int(current_value)
+		#for example "55"
 	if current_argument in ("-f", "--folder_name"):
 		folder = str(current_value)
-	if current_argument in ("-m", "--model_path"):
-		model_path = str(current_value)
-	if current_argument in ("-h", "--help"):
-		print("Downsizes contents of a uCT recon folder to 243x243 and saves it to a separate directory. -f OR --folder= specifies the folder name (e.g. N2_w_01) and -m OR -max_index specifies the max index of that folder")
+		#for example "N2_w_01"
+		#this string is used for both defining the folder name, as well as iterating through the files contained in that folder
 
+#generate file paths
+
+in_dir = '/base_directory/'
 input_dir = in_dir + 'Reconstructions/' + folder
 output_dir = in_dir + 'TF/' + folder
-
-# if not os.path.exists(output_dir):
-#     try:
-#         os.makedirs(output_dir)
-#     except OSError as e:
-#         if e.errno != errno.EEXIST:
-#             raise
 
 print('Input directory: ' + input_dir)
 print('Output directory: ' + output_dir)
@@ -64,6 +54,8 @@ for time_index in range(max_index):
 		data = np.array(hdf['reconstructed'])
 
 	#determine slice size and downscale factor
+    #determine padding, if needed
+    
 	ds_factor_base = x // 243
 	ds_factor_remainder = x % 243
 	if ds_factor_remainder > 0:
@@ -72,7 +64,6 @@ for time_index in range(max_index):
 		ds_factor_remainder = padded_x - x
 		padding_l = ds_factor_remainder // 2
 		padding_r =  ds_factor_remainder - padding_l
-		#prep padded output array
 	else:
 		padded_x = x
 		padded_y = y
@@ -90,12 +81,18 @@ for time_index in range(max_index):
 		padding_z_d = 0
 		padding_z_u = 0
 	z_out = padded_z // ds_factor_base
+    
+    #create padded array
+    
 	data_padded = np.zeros([padded_z,padded_x,padded_x])
 	print(data_padded.shape)
 	print(padding_l)
 	print(padding_r)
 	print(padding_z_d)
 	print(padding_z_u)
+    
+    #populate padded array 
+    
 	if padding_z_d > 0 and padding_z_u > 0 and padding_l > 0 and padding_r > 0:
 		data_padded[padding_z_d:-padding_z_u,padding_l:-padding_r,padding_l:-padding_r] = data
 	elif padding_z_d == 0 and padding_z_u == 0 and padding_l > 0 and padding_r > 0:
@@ -115,6 +112,9 @@ for time_index in range(max_index):
 	y_out = y_in // factor
 	x_out = x_in // factor
 	data_out = np.empty([z_out,y_out,x_out])
+    
+    #use numba to rapidly do the downscaling using a moving average algorithm
+    
 	@numba.jit(nopython=True)
 	def gofast(data, data_out, factor, z_out, y_out, x_out):
 		for z in range(z_out):
@@ -132,11 +132,17 @@ for time_index in range(max_index):
 		                        sum += data_padded[pos]
 		            data_out[z,y,x] = sum/(factor*factor*factor)
 		return(data_out)
+    
+    #populate the output array
+    
 	data_out = gofast(data, data_out, factor, z_out, y_out, x_out)
+    
     #add 'color' axis
+    
 	data_out = data_out[:,:,:,np.newaxis]
 
     #save downsized file
+    
 	print('saving file ' + str(time_index+1) + ' of ' + str(max_index))
 	filename_out = output_dir + '/' + folder + '_reconstructed_' + str(time_index + 1) + '.h5'
 	with h5py.File(filename_out, 'a') as hdf2:
